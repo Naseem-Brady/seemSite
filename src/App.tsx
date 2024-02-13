2b70568bc1d4864000eaf18f3ec15dab378acbee
Naseem-Brady <89871978+Naseem-Brady@users.noreply.github.com>
2024-02-13 03:29:46 +0000

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,7 @@ export default function Page() {
         <>
           <img src="https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExODl6cW45ajYwNWxlaTVoYmducjV3bGM4aDh6OW5mM2N5MXltaDlneSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/i1ifSvtaxp4YSWX4o4/giphy.gif" />
           <div className="my-4 text-4xl font-bold">AHHHHHH I LOVE YOU !!! :DDD ;))</div>
+          <a href="https://docs.google.com/document/d/1LB7_9Nj6KnKlg_vs4gMAW5LfeqXnuqAHlZh119MxFAU/edit?usp=sharing"> ...Click here. It's letter for you :) </a>
         </>
       ) : (
         <>
